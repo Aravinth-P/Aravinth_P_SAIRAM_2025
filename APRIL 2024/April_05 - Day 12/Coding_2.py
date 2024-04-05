e0c56fd6149a3fe75509e22c399cb4a959d7b609
Aravinth-P <157289089+Aravinth-P@users.noreply.github.com>
2024-04-05 18:24:46 +0530

    Create Coding_2.py

---
+++ APRIL 2024/April_05 - Day 12/Coding_2.py
@@ -0,0 +1,9 @@
+''' Write a program to Check Whether a Number is Positive, Negative, or Zero. '''
+
+n=int(input())
+if n==0:
+  print("Zero")
+elif n>0:
+  print("Positive")
+else:
+  print("Negative")
