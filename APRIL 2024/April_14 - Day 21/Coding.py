64affa08882be8ac887152a0f602056be848cf18
Aravinth-P <157289089+Aravinth-P@users.noreply.github.com>
2024-04-14 15:49:48 +0530

    Update Coding.py

--- APRIL 2024/April_14 - Day 21/Coding.py
+++ APRIL 2024/April_14 - Day 21/Coding.py
@@ -7,3 +7,30 @@ if s in "AEIOUaeiou":
   print(s,"is a vowel")
 else:
   print(s,"is a consonant")
+    
+
+
+      *
+     ***
+    *****
+   *******
+  *********
+   *******
+    *****
+     ***
+      *
+
+n=int(input())
+a=1
+for i in range(n):
+    for _ in range(n-i+1):
+        print(" ",end="")
+    for j in range(2*i+1):
+        print("*",end="")
+    print()
+for i in range(n-2,-1,-1):
+    for _ in range(n-i+1):
+        print(" ",end="")
+    for j in range(2*i+1):
+        print("*",end="")
+    print()
