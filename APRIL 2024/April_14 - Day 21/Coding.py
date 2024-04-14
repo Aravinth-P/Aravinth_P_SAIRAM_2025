e732feb98729e5375b52d9412038acff0b4bcc16
Aravinth-P <157289089+Aravinth-P@users.noreply.github.com>
2024-04-14 15:34:16 +0530

    Create Coding.py

---
+++ APRIL 2024/April_14 - Day 21/Coding.py
@@ -0,0 +1,9 @@
+''' 1. Program to find an alphabet by vowel or consonant
+    2. Dimaond star pattern printing '''
+
+
+s=input().strip()
+if s in "AEIOUaeiou":
+  print(s,"is a vowel")
+else:
+  print(s,"is a consonant")
