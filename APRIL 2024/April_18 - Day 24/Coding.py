de6c2a91645ba188a594077e3b5d9de05471648c
Aravinth-P <157289089+Aravinth-P@users.noreply.github.com>
2024-04-18 22:02:53 +0530

    Update Coding.py

--- APRIL 2024/April_18 - Day 24/Coding.py
+++ APRIL 2024/April_18 - Day 24/Coding.py
@@ -2,3 +2,20 @@
     2.Write a Python program to remove duplicates from a list '''
 
 
+def gcd(a,b):
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
+
+a,b=map(int,input().split())
+print(gcd(a,b))
+
+
+
+l=list(map(int,input().split()))
+r=[]
+for i in l:
+    if i not in r:
+        r.append(i)
+print(*r)
