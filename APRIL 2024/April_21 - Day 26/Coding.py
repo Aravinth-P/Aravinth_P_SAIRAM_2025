e2be5a436c3b14317d5e00b36d1b94e058f7bdcf
Aravinth-P <157289089+Aravinth-P@users.noreply.github.com>
2024-04-21 21:40:59 +0530

    Update Coding.py

--- APRIL 2024/April_21 - Day 26/Coding.py
+++ APRIL 2024/April_21 - Day 26/Coding.py
@@ -7,8 +7,8 @@ print(max(a,b))
 print(min(a,b))
 
 
-a,b,c=map(int,input().split())
-c=str(c)
+a,b,c=map(str,input().split())
+a,b=int(a),int(b)
 s={
   '+' : lambda a,b : a+b,
   '-' : lambda a,b : a-b,
