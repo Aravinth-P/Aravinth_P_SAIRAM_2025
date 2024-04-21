fc547424fd97f092eefc88cb59c34123f2a815c3
Aravinth-P <157289089+Aravinth-P@users.noreply.github.com>
2024-04-21 21:32:26 +0530

    Create Coding.py

---
+++ APRIL 2024/April_21 - Day 26/Coding.py
@@ -0,0 +1,10 @@
+''' Write a Program to find the Maximum and minimum of two numbers without using any loop or condition. 
+Write a Program for simple calculator. '''
+
+
+a,b=map(int, input().split())
+print(max(a,b))
+print(min(a,b))
+
+
+
